ef324bcbc3744f8bb3128d663510176f7e82c698
Jacegk1999 <Jacegk1999@gmail.com>
2022-03-07 17:11:42 -0800

    remember to put things into an array when pulling multipul items. also make smaller commits. everything is rendering

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
-export default function Animal() {
-  return <></>;
+export default function Animal({ name, say, top, left, type }) {
+  return (
+    <>
+      <div className="animal" style={{ top, left }}>
+        <img alt={name} src={`${process.env.PUBLIC_URL}/animals/${type}.svg`} />
+        <span className="name">{name}</span>
+        <span>{say}</span>
+      </div>
+    </>
+  );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './Main.css';
+import Animal from '../Animal/Animal.js';
+import backgroundImg from '../../background.png';
+import { animals } from '../../data.js';
 
 export default function Main() {
-  return <main></main>;
+  return (
+    <main style={{ backgroundImage: `url(${backgroundImg})` }}>
+      {animals.map((item) => (
+        <Animal key={item.name} {...item} />
+      ))}
+    </main>
+  );
 }
