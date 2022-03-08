23e4397d02072b5c2b3aee50b76bb18cc8ca3e37
Jacegk1999 <Jacegk1999@gmail.com>
2022-03-07 18:06:58 -0800

    forgot an s for says

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export default function Animal({ name, say, top, left, type }) {
+export default function Animal({ name, says, top, left, type }) {
   return (
     <>
       <div className="animal" style={{ top, left }}>
         <img alt={name} src={`${process.env.PUBLIC_URL}/animals/${type}.svg`} />
         <span className="name">{name}</span>
-        <span>{say}</span>
+        <span>{says}</span>
       </div>
     </>
   );
