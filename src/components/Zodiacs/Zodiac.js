48f7a903bf5b563b624d028517ba5778a7d370c1
Jacegk1999 <Jacegk1999@gmail.com>
2022-03-08 14:30:44 -0800

    started adding zodiac info to this and i just removed it

--- src/components/Zodiacs/Zodiac.js
+++
@@ -1,9 +0,0 @@
-export default function ZodiacRender({ name, dates }) {
-  return (
-    <div className="zodiac-render">
-      <img alt={name} src={`../assets/${name}.png`} />
-      <span className="name">{name}</span>
-      <span>{dates}</span>
-    </div>
-  );
-}
